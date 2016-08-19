9a2102d9a3350ae6fcbee48e169aee8edd93a736
Stephen Hosom <0xhosom@gmail.com>
2016-08-19 11:17:49 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='pymandrake',
+		version='0.1',
+		description='Python Mandrake Plugins',
+		author='Stephen Hosom',
+		author_email='0xhosom@gmail.com',
+		url='https://github.com/hosom/pymandrake',
+		packages=['pymandrake'],
+		install_requires=[
+			'pyjsonrpc',
+		],
+		)
